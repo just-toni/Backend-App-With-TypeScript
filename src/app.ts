18e8746711033fc33b02dcb9448c410fcd1c96a7
just-toni <toniadeniyi14@gmail.com>
2024-09-11 20:55:07 -0300

    updated file structure

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import routes from './routes/authRoutes.js'
+import routes from './src/routes/authRoutes.js'
 import bodyParser from 'body-parser';
 
 const app = express();
